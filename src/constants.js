35b68cb1d33a3e01904436fe4c837c1e7292e495
shivakokkula2 <shivakokkula@varcsoft.com>
2025-07-13 00:15:20 +0530

    hfdhfdk

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // const SERVER_URL="http://localhost:8000"
-const SERVER_URL="https://questgen-backend.onrender.com"
+const SERVER_URL="https://quizo-backend-aek5.onrender.com"
 const config = { SERVER_URL };
 
 export default config;
